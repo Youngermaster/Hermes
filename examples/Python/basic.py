8a7472520331bdf9a9a7f28646565c351856e483
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2022-01-18 13:25:33 -0500

    Project initialized

---
+++ examples/Python/basic.py
@@ -0,0 +1,14 @@
+def add(a, b):
+    return a + b
+
+
+def minus(a, b):
+    return a - b
+
+
+if __name__ == "__main__":
+    print("Hello world")
+    a = 12
+    b = 13
+    print(add(a, b))
+    print(minus(a, b))
